e5880170a4a5e3221216f4fac588c4288af047c9
xiami <891627097@qq.com>
2021-02-20 23:16:32 +0800

    nacos分布式配置中心 1、通过代码获取配置环境     @Autowired     private ConfigurableApplicationContext configurableApplicationContext;
        // 从上下文中读取配置
        @GetMapping(value = "/hi")
        public String hi(){
            String property = configurableApplicationContext.getEnvironment().getProperty("user.name");
            return property;
        }
    2、bootstrap.properties优先级最高
    # 这里的应用名对应 Nacos Config 中的 Data ID，实际应用名称以配置中心的配置为准
    spring.application.name=nacos-provider-config
    # 指定查找名为 nacos-provider-config.yaml 的配置文件
    spring.cloud.nacos.config.file-extension=yaml
    # Nacos Server 的地址
    spring.cloud.nacos.config.server-addr=127.0.0.1:8848
    3、客户端添加依赖
            <!-- Nacos Config 分布式配置中心客户端  start-->
            <dependency>
                <groupId>org.springframework.cloud</groupId>
                <artifactId>spring-cloud-starter-alibaba-nacos-config</artifactId>
            </dependency>
            <!-- Nacos Config 分布式配置中心客户端  end-->

--- hello-spring-cloud-alibaba-nacos-provider/src/main/java/com/funtl/hello/spring/cloud/alibaba/nacos/provider/controller/NacosProviderController.java
+++ hello-spring-cloud-alibaba-nacos-provider/src/main/java/com/funtl/hello/spring/cloud/alibaba/nacos/provider/controller/NacosProviderController.java
@@ -1,7 +1,9 @@
 package com.funtl.hello.spring.cloud.alibaba.nacos.provider.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NacosProviderController {
 
+    //这个是一次性的，加载到内存，配置文件修改了，这个值还是不变
     @Value("${server.port}")
     private String port;
 
     @GetMapping(value = "/echo/{message}")
     public String echo(@PathVariable(value = "message")String message){
         return "hello Nacos： "+message+" i am from port:"+port;
+    }
+
+    // 注入配置文件上下文。可以动态获取到配置文件修改的值
+    @Autowired
+    private ConfigurableApplicationContext configurableApplicationContext;
+
+    // 从上下文中读取配置
+    @GetMapping(value = "/hi")
+    public String hi(){
+        String property = configurableApplicationContext.getEnvironment().getProperty("user.name");
+        return property;
     }
 }
